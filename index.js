f6130886192cf08ac891be0639d7ba047ba4452a
lgtkaushalya <lgtkaushalya@gmail.com>
2020-10-04 15:46:03 +0530

    Added the replace-angular-inbuilt-directives to eslint

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 module.exports = {
     rules: {
       "ng-directive-template-naming" : require('./lib/rules/ng-directive-template-naming'),
-      "isolate-scope" : require('./lib/rules/isolate-scope')
+      "isolate-scope" : require('./lib/rules/isolate-scope'),
+      "replace-angular-inbuilt-directives" : require('./lib/rules/replace-angular-inbuilt-directives')
     }
 };
