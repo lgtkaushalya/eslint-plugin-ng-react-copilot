6ca741be7b2cee63eed49d70a1a0523e3e057982
lgtkaushalya <lgtkaushalya@gmail.com>
2020-10-10 23:32:55 +0530

    Added replace-angular-scope-references-in-react-components rule

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ module.exports = {
     rules: {
       "ng-directive-template-naming" : require('./lib/rules/ng-directive-template-naming'),
       "isolate-scope" : require('./lib/rules/isolate-scope'),
-      "replace-angular-inbuilt-directives" : require('./lib/rules/replace-angular-inbuilt-directives')
+      "replace-angular-inbuilt-directives" : require('./lib/rules/replace-angular-inbuilt-directives'),
+      "replace-angular-scope-references-in-react-components" : require('./lib/rules/replace-angular-scope-references-in-react-components')
     }
 };
