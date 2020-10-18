2613b1f2f467c6fb896f17181fac9b3bb0f79cd4
lgtkaushalya <lgtkaushalya@gmail.com>
2020-10-18 23:51:25 +0530

    Added prop type validation and directive replacement suggestion

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ module.exports = {
       "ng-directive-template-naming" : require('./lib/rules/ng-directive-template-naming'),
       "isolate-scope" : require('./lib/rules/isolate-scope'),
       "replace-angular-inbuilt-directives" : require('./lib/rules/replace-angular-inbuilt-directives'),
-      "replace-angular-scope-references-in-react-components" : require('./lib/rules/replace-angular-scope-references-in-react-components')
+      "replace-angular-scope-references-in-react-components" : require('./lib/rules/replace-angular-scope-references-in-react-components'),
+      "extract-directive-and-component-information" : require('./lib/rules/extract-directive-and-component-information')
     }
 };
